d6144d6bf890efadd1af52be24810f55baabfd48
Haaris <haarisj22@hotmail.ca>
2020-08-10 09:20:38 -0400

    Remove mongodb connection from models files

--- models/campground.js
+++ models/campground.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp_v3', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
-
 //Schema Setup
 const campgroundSchema = new mongoose.Schema({
     name: String,
--- models/comment.js
+++ models/comment.js
@@ -1,12 +1,5 @@
 const mongoose = require("mongoose");
 
-mongoose.connect('mongodb://localhost:27017/yelp_camp_v3', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log('Connected to DB!'))
-.catch(error => console.log(error.message));
-
 const commentSchema = mongoose.Schema({
     text: String,
     author: {
