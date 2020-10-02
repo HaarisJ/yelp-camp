952d814be0b601bd9bdfca63944a02086c8893e3
Haaris <haarisj22@hotmail.ca>
2020-10-02 14:27:28 -0400

    Add campground star rating functionality

--- app.js
+++ app.js
@@ -59,6 +59,11 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(3000, function(){
     console.log("YelpCamp Server Running on " + process.env.PORT);
 });
+
+
+// app.listen(process.env.PORT, process.env.IP, function(){
+//     console.log("YelpCamp Server Running on " + process.env.PORT);
+// });
--- models/campground.js
+++ models/campground.js
@@ -2,23 +2,24 @@ const mongoose = require("mongoose");
 
 //Schema Setup
 const campgroundSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String,
-    price: String,
-    author: {
-        id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User"
-        },
-        username: String
+  name: String,
+  image: String,
+  description: String,
+  price: String,
+  rating: { type: Number, min: 1, max: 5 },
+  author: {
+    id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
-    comments: [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Comment"
-        }
-    ]
+    username: String,
+  },
+  comments: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Campground", campgroundSchema);
